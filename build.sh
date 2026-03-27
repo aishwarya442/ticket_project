88fe8fbdf7b9c8e39cee6ef5d7d22dfd937fa495
Aishwarya Shetty <ash.18shetty04@gmail.com>
2026-03-28 01:41:32 +0530

    Fixed Replit Build files

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 set -o errexit
 
 pip install -r requirements.txt
-
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Start the server!
+gunicorn ticket_project.wsgi:application --bind 0.0.0.0:8000
